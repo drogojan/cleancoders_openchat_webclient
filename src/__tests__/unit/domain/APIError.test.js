0c1abf3571816f17501727b67c7a6e63f6bed4df
Dragos Rogojan <dragos.rogojan@gmail.com>
2020-02-01 02:22:50 +0200

    Fixed failing test

--- src/__tests__/unit/domain/APIError.test.js
+++ src/__tests__/unit/domain/APIError.test.js
@@ -7,11 +7,11 @@ describe('APIError', () => {
         url: `${process.env.REACT_APP_API_URL}users`,
         method: 'post'
       },
-      data: 'Username already in use.'
+      data: { message: 'Username already in use.' }
     }
     const error = new APIError(response)
 
-    expect(error.name).toBe(response.data)
-    expect(error.message).toBe(`POST /users`)
+    expect(error.name).toBe(response.data.message)
+    expect(error.message).toBe(`POST /api/users`)
   })
 })
