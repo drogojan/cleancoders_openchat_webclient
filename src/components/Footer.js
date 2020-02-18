978e2954f45bb357e8f8da9c3c0f4cbcc1b6b48a
Dragos Rogojan <dragos.rogojan@gmail.com>
2020-02-18 14:08:09 +0200

    Commented logo and powered by

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ class Footer extends Component {
         <div className='container'>
           <div className='row'>
             <div className='col mr-auto'>
-              <a href='https://codurance.com/' target='_blank' rel='noopener noreferrer'>
+              {/* <a href='https://codurance.com/' target='_blank' rel='noopener noreferrer'>
                 Powered by Codurance.com
-              </a>
+              </a> */}
             </div>
             <div className='col text-right'>
-              Crafted with <i className='fa fa-heart text-danger' />
+              {/* Crafted with <i className='fa fa-heart text-danger' /> */}
             </div>
           </div>
         </div>
